cb2a771bda7e8cf889e08ffc59fb3fafab801d4d
seyi4mi <seyi4mi99@gmail.com>
2021-11-11 20:19:22 +0800

    Remove dots, made draggable false

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -52,39 +52,16 @@ const cardContainer = {
 export default class PauseOnHover extends Component {
 	render() {
 		const settings = {
-			dots: true,
+			dots: false,
 			infinite: true,
 			slidesToShow: 4,
-			slidesToScroll: 2,
+			slidesToScroll: 4,
 			autoplay: true,
-			autoplaySpeed: 3000,
+			autoplaySpeed: 0,
+			speed: 7000,
 			pauseOnHover: true,
-			responsive: [
-				{
-					breakpoint: 1024,
-					settings: {
-						slidesToShow: 3,
-						slidesToScroll: 1,
-						infinite: true,
-						dots: true,
-					},
-				},
-				{
-					breakpoint: 600,
-					settings: {
-						slidesToShow: 2,
-						slidesToScroll: 1,
-						initialSlide: 2,
-					},
-				},
-				{
-					breakpoint: 480,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1,
-					},
-				},
-			],
+			draggable: false,
+			cssEase: "linear",
 		}
 		return (
 			<div className="cards" style={cardContainer}>
